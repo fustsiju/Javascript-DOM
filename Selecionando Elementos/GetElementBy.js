d4bfe5f2285e6b25d633a5d3f1b295afa4fbbc4c
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2022-06-28 09:51:14 -0300

    Add files via upload

---
+++ Selecionando Elementos/GetElementBy.js
@@ -0,0 +1,14 @@
+getElementById
+
+//const element = document.getElementById('')
+//console.log(element)
+
+getElementByClassName
+
+//const element = document.getElementsByClassName('')
+//console.log(element)
+
+getElementByTagName
+
+//const element = document.getElementsByTagName('')
+//console.log(element)
---
+++ Selecionando Elementos/query.js
@@ -0,0 +1,11 @@
+// querySelector = Pega o
+
+//const element = document.querySelector('.div')    --> Pega pela classe CSS, pega apenas o primeiro que achar.
+//console.log(element)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//querySelectorAll
+
+//const element = document.querySelectorAll('')     --> Pela pela classe CSS, pega todos que achar.
+//console.log(element)

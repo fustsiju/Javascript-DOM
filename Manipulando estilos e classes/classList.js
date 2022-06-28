43ec01b3500094f6c1f0176520913cd377336615
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2022-06-28 09:51:55 -0300

    Add files via upload

---
+++ Manipulando estilos e classes/classList.js
@@ -0,0 +1,9 @@
+//Alterando estilos
+//classList
+const element = document.querySelector('body')
+
+element.classList.add('active')     //Adicionando classe active no body
+
+element.classList.remove('active')      //removendo classe active do body
+
+element.classList.toggle('active')      //Caso não tenha ele adiciona ao body, caso tenha ele remove do body

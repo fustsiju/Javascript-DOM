5d31e6e86daac546ab24c7193ca1865b1bb69f48
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2022-06-28 09:53:16 -0300

    Add files via upload

---
+++ Praticando/modal.js
@@ -0,0 +1,16 @@
+const openModalButton = document.getElementById('openModal')
+
+const modalWrapper = document.querySelector('modal-wrapper')
+
+openModalButton.onclick = function(){
+    modalWrapper
+        .classList.remove('invisible')
+}
+
+document.addEventListener('keydown', function(event){
+    const isEscKey = event.key === 'Escape'
+
+    if(isEscKey){
+        modalWrapper.classList.add('invislbe')
+    }
+})
